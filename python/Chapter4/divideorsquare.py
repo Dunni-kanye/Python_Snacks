9cda9b7066595876762f66f094c1d435e7d40df4
Dunni-Kanye <dunniade18@gmail.com>
2024-07-23 09:01:48 +0100

    my codes

---
+++ python/Chapter4/divideorsquare.py
@@ -0,0 +1,14 @@
+import math
+
+def divide_or_square(number):
+	if number % 5 == 0:
+		return number**0.5
+
+
+	else:
+		return number % 5
+
+print(divide_or_square(26))
+print(divide_or_square(25))
+
+	
