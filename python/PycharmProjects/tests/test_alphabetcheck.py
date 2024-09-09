80945873e81c62c74419f25b8ee161e51be9038e
Dunni-Kanye <dunniade18@gmail.com>
2024-09-09 03:11:58 +0100

    my codes

---
+++ python/PycharmProjects/tests/test_alphabetcheck.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from myPythonCode.classTask.alphabetcheck import alphabet_dict
+class TestAlphabet(TestCase):
+    def test_alphabet(self):
+        self.assertEqual(alphabet_dict("bimbola"), {'a': 1, 'b': 2, 'i': 1, 'm': 1, 'l': 1, 'o': 1})
+        self.assertEqual(alphabet_dict("Bimbola"), {'a': 1, 'b': 2, 'i': 1, 'm': 1, 'l': 1, 'o': 1})
---
+++ python/PycharmProjects/tests/test_cubedtask.py
@@ -0,0 +1,11 @@
+#from unittest import TestCase
+
+#from classTask.cubedtask1 import list_to_cubed_dict
+
+
+
+#class Testcubedtask1(TestCase):
+    #def test_cubed_dict(self):
+        #self.assertEqual(list_to_cubed_dict([1,2,3,4,5]), {1:1, 2:8,3:27,4:64, 5:125})
+
+
